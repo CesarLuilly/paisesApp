ecc8e0cf728b17235726ec002270ff3fd709eac6
CesarLuilly <cesarluilly.garcia@hotmail.com>
2021-03-15 14:15:24 -0500

    Video 102 - Componente para buscar pais

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { FormsModule } from '@angular/forms';
+
 
 
 
 @NgModule({
   declarations: [PorCapitalComponent, PorPaisComponent, PorRegionComponent, VerPaisComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports:[
     //              //Se van a utilizar de forma global al exportarlo.
