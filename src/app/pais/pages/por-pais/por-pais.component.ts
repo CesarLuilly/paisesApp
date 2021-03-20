1bee234dec238185c16f96ed705771fcf335bff5
CesarLuilly <cesarluilly.garcia@hotmail.com>
2021-03-20 14:25:08 -0500

    Video 104 - Trata errores

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -10,6 +10,7 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   public termino : any = "Hola Mundo";
+  public hayError : boolean = false;
 
   constructor(
     //              //inyeciond e servicio.
@@ -19,12 +20,20 @@ export class PorPaisComponent {
   
   public buscar(){
 
+    this.hayError = false;
+
     this._paisService.buscarPais(this.termino).subscribe(
-      resp => {
+      (resp) => {
         console.log(resp);
+      }, 
+      (err) => {
+        this.hayError = true;
+        console.log('Error');
+        console.log(err);
       }
     );
 
+    
     console.log(this.termino);
   }
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +26,18 @@ export class PaisService {
   {
     const url = `${ this._apiUrl }/name/${ termino }`;
     
+    // //              //OTRA MANERA DE TRATAR UN ERROR.
+    // return this._http.get( url )
+    //   .pipe(
+    //     //          //Funcion para lanzar un error.
+    //     //          //Lo cual retorna un observable y convertimos ese 
+    //     //          //  observable en un nuevo observable a traves de 
+    //     //          //  "of([])"
+    //     //          //En otra palabras aqui atrapa el error y devuelve un
+    //     //          //  arreglo vacio.
+    //     catchError(err => of([]))   
+    //   );
+
     return this._http.get( url );
   }
 }
